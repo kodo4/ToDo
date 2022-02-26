af7e47332eb4b9ef3a3649d44c38d45f88f15fe2
Роман Кодочигов <kodochigovra@mail.ru>
2022-02-26 13:35:14 +0300

    ready lesson_2

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +129,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.Users'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
