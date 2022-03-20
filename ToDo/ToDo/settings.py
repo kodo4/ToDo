e13d8013cdde9ddbfd2349781f681482fbd5e170
Роман Кодочигов <kodochigovra@mail.ru>
2022-03-20 17:58:31 +0300

    ready lesson-5

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my
     'users',
-    # others
     'Projectapp',
+    # others
     'rest_framework',
     'corsheaders',
     'django_filters',
--- ToDo/frontend/src/components/Menu.js
+++ ToDo/frontend/src/components/Menu.js
@@ -1,12 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Menu = ({menu}) => {
     return (
         <nav className="two">
             <ul>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
+                <li><Link to='/'>Users</Link></li>
+                <li><Link to='/project'>Projects</Link></li>
+                <li><Link to='/todo'>ToDo</Link></li>
             </ul>
         </nav>
 
