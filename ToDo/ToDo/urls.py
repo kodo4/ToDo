39cbf01224fa2e12e1bec6694a5e089aff3181e9
Роман Кодочигов <kodochigovra@mail.ru>
2022-03-29 18:54:36 +0300

    ready lesson_6

--- ToDo/ToDo/urls.py
+++ ToDo/ToDo/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import views
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet
 from Projectapp.views import ProjectModelViewSet, ToDoViewSet
+from rest_framework_simplejwt.views import \
+    TokenObtainPairView, \
+    TokenRefreshView, \
+    TokenVerifyView
+
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
@@ -29,4 +35,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    # path('api-token-auth/', views.obtain_auth_token),
+
+    path('api/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
 ]
